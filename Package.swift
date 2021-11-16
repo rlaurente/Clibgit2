10e5cd7150438f1117e7bb2d46782d140d2c697f
Developer <developer@dev-mac.local>
2021-11-16 15:22:34 -0500

    change download path

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "Clibgit2",
-			url: "https://github.com/light-tech/LibGit2-On-iOS/releases/download/v1.3.0/Clibgit2.xcframework.zip",
-			checksum: "bb7fafd7f461ce5669443d73ea8761b3db9a645d07085b37966c96b00b7fac95"
+			url: "https://github.com/rlaurente/Clibgit2/blob/master/Clibgit2.xcframework.zip?raw=true"
 		),
 	]
 )
